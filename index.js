9e3aa4c3fc4e92c0b3f39b3004c88d77c5cc41cf
nichmatt <nicholas.official17@gmail.com>
2024-03-30 07:51:59 +0700

    feat: 🎸 initial commit
    initial commmit

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
-// connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
